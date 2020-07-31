091f60a05ebfb3c63958ddcc08c7d29622297712
Oscar Rodriguez <oscar@verify.as>
2020-07-31 13:58:50 +0200

    created internal cache

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ import { querySudo as query } from '@lblod/mu-auth-sudo';
 
 import bodyParser from 'body-parser';
 
+const cache = {
+
+};
+
 // parse application/json
 app.use(bodyParser.json());
 
 app.post('/getInfo', async (req, res) => {
   const {term, language = 'nl'} = req.body;
+  if(cache[term] && cache[term][language]) {
+    return res.json(cache[term][language]);
+  }
   const queryResult = await query(`
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?label ?comment WHERE {
@@ -22,10 +29,20 @@ app.post('/getInfo', async (req, res) => {
     }
   `);
   const info = queryResult.results.bindings[0];
-  res.json({
+  if(!info) {
+    return res.json({error: 'No info in the db'})
+  }
+  const infoFormatted = {
     label: info.label ? info.label.value : '',
     comment: info.comment ? info.comment.value : '',
-  });
+  }
+  if(!cache[term]) {
+    cache[term] = {}
+    cache[term][language] = infoFormatted;
+  } else {
+    cache[term][language] = infoFormatted;
+  }
+  res.json(infoFormatted);
 });
 
 app.get('/test', async (req, res) => {
