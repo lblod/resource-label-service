c5b44b9f3e11dc70d6cdfc00d8cf3edd68a71886
Oscar <lagartoverde97@gmail.com>
2020-08-14 12:48:31 +0200

    removed cache and unused dependencies

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-import { app, errorHandler, sparqlEscapeUri, sparqlEscapeString } from 'mu';
-import { querySudo as query } from '@lblod/mu-auth-sudo';
-
-import bodyParser from 'body-parser';
-
-const cache = {
-
-};
-
-// parse application/json
-app.use(bodyParser.json());
+import { app, errorHandler, sparqlEscapeUri, sparqlEscapeString, query } from 'mu';
 
 app.get('/getInfo', async (req, res) => {
   const {term, language = 'nl'} = req.query;
-  if(cache[term] && cache[term][language]) {
-    return res.json(cache[term][language]);
+  if(!term) {
+    return res.json({error: 'No term specified'});
   }
+  console.log(term)
   const queryResult = await query(`
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     SELECT ?label ?comment WHERE {
-
       ${sparqlEscapeUri(term)} rdfs:label ?label.
       FILTER (lang(?label) = ${sparqlEscapeString(language)})
-
       OPTIONAL {
         ${sparqlEscapeUri(term)} rdfs:comment ?comment.
         FILTER (lang(?comment) = ${sparqlEscapeString(language)})
@@ -36,16 +25,10 @@ app.get('/getInfo', async (req, res) => {
     label: info.label ? info.label.value : '',
     comment: info.comment ? info.comment.value : '',
   };
-  if(!cache[term]) {
-    cache[term] = {};
-    cache[term][language] = infoFormatted;
-  } else {
-    cache[term][language] = infoFormatted;
-  }
-  
   res.setHeader('MU_AUTH_CACHE_KEYS', JSON.stringify([{"name": "getInfo", parameters:[]}]));
   res.json(infoFormatted);
 });
- 
+
+
 
 app.use(errorHandler);
