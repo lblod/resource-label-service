d452b3d1013c7bc920f5ebcab2eeb33d77aaa079
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-10-01 15:46:14 +0200

    added support for 'nil' language

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 import { app, errorHandler, sparqlEscapeUri, sparqlEscapeString, query, uuid } from 'mu';
 
+
+let DEFAULT_LANGUAGE;
+
+// If no default language is provided, nl will be used as default if not specified in query.
+if(!process.env.DEFAULT_LANGUAGE){
+  DEFAULT_LANGUAGE = 'nl';
+}
+// The language is specicified, we will use the latter as default if not specified in query.
+else if(!process.env.DEFAULT_LANGUGAGE == 'nil'){
+  DEFAULT_LANGUAGE = DEFAULT_LANGUAGE;
+}
+//else The language is nil, we don't filter on language.
+
+console.log(`The default language will be ${DEFAULT_LANGUAGE}`);
+
 app.get('/info', async (req, res) => {
-  const {term, language = 'nl'} = req.query;
+  const {term, language = DEFAULT_LANGUAGE } = req.query;
 
   if(!term) {
     const jsonApiError = generateJsonApiError({
@@ -14,15 +29,18 @@ app.get('/info', async (req, res) => {
     return res.json(jsonApiError);
   }
 
+  let languageFilterLabel = language ? `FILTER (lang(?label) = ${sparqlEscapeString(language)})` : '';
+  let languageFilterComment = language ? `FILTER (lang(?comment) = ${sparqlEscapeString(language)})` : '';
+
   const queryResult = await query(`
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     SELECT ?label ?comment WHERE {
       ${sparqlEscapeUri(term)} (rdfs:label|skos:prefLabel) ?label.
-      FILTER (lang(?label) = ${sparqlEscapeString(language)})
+      ${languageFilterLabel}
       OPTIONAL {
         ${sparqlEscapeUri(term)} (rdfs:comment|skos:definition) ?comment.
-        FILTER (lang(?comment) = ${sparqlEscapeString(language)})
+        ${languageFilterComment}
       }
     }
   `);
